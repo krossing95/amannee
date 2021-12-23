4e7470aa20fc8cf5f9591f6da5da63e6c38958f4
xoese kalenu <kalenuxoesekwasi@gmail.com>
2021-12-22 23:55:47 -0800

    new commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+    origin: ['https://amannee.netlify.app:3000'],
+    credentials: true
+}));
+
+const router = require('./routers/router');
+
+const PORT = process.env.PORT || 4100;
+
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+    res.send('News proxy api');
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
---
+++ routers/router.js
@@ -0,0 +1,7 @@
+const { fetchNews } = require('../controllers/controller');
+
+const router = require('express').Router();
+
+router.get('/', fetchNews);
+
+module.exports = router;
